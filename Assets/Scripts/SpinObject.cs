d24235d19b27ce1edfb734369fb2bb2c0dd1b18d
sam <samuel.haddad@designinteractive.net>
2014-05-26 17:23:00 -0400

    Introduction of Unity Project

---
+++ Assets/Scripts/SpinObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinObject : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+	
+	}
+}
---
+++ Assets/Scripts/VirtualButtonPress.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirtualButtonPress : MonoBehaviour 
+{
+    public bool buttonPressed = false;
+    public bool useSphereCast = false;
+    public bool playSoundOnPress = false;
+    public AudioClip soundToPlayOnPress;
+	//private QualityController qualityController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//qualityController = QualityController.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        ProcessTouches();
+        HandleButtonPressed();
+	}
+
+
+    void ProcessTouches()
+    {
+		// For Mouse Input
+
+		if (Input.GetMouseButtonDown(0))
+		{
+            RaycastAtScreenPosition(Input.mousePosition);
+		}
+		
+		// For Touches
+		foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                RaycastAtScreenPosition(touch.position);
+
+            }
+        }
+    }
+
+    private void RaycastAtScreenPosition(Vector2 screenPosition)
+    {
+        var ray = Camera.main.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+        bool castHitSomething = false;
+
+        if (useSphereCast)
+        {
+            castHitSomething = Physics.SphereCast(ray, 1.0f, out hit, 300);
+            print("SphereCast Hit : " + hit.collider.name);
+        }
+        else
+        {
+            castHitSomething = Physics.Raycast(ray, out hit);
+        }
+
+        if (castHitSomething)
+        {
+            if (hit.collider == this.collider)
+            {
+                buttonPressed = true;
+            }
+        }
+    }
+
+    void HandleButtonPressed()
+    {
+        if(!buttonPressed)
+            return;
+
+        ITappable tappedObject = this.GetComponent(typeof(ITappable)) as ITappable;
+
+        if (tappedObject != null)
+        {
+            tappedObject.Tap();
+            if (playSoundOnPress)
+            {
+                audio.PlayOneShot(soundToPlayOnPress);
+            }
+        }        
+
+        buttonPressed = false;
+
+	}
+}
