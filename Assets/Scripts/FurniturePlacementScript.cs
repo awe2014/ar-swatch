0f2d25724dffd9d65ee1f43c83273b2396240225
sam <samuel.haddad@designinteractive.net>
2014-05-26 21:26:05 -0400

    Added furniture placement

---
+++ Assets/Scripts/FurniturePlacementScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurniturePlacementScript : MonoBehaviour, ITappable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Tap()
+    {
+        Application.LoadLevel("furniturePlacement");
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void LoadFurniturePlacement()
+    {
+        Application.LoadLevel("furniturePlacement");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckAndroidExit();
+	
+	}
+
+    private void CheckAndroidExit()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
